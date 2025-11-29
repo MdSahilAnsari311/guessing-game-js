c0c68c928554bc28c22d4970419823dfe879f467
MdSahilAnsari311 <sahilansari66965@gmail.com>
2025-11-29 15:49:49 +0530

    Intial commit: add index.html and app.js

---
+++ app.js
@@ -0,0 +1,22 @@
+// Guessing Game
+const max = prompt("Enter the max number:");
+
+const random = Math.floor(Math.random()*max)+1;
+
+let guess = prompt("Guess the number:");
+
+while(true){
+    if(guess == "quit"){
+        console.log("User quit.");
+        break;
+    }
+
+    if(guess == random){
+        console.log("Your guess is right! congrats!!. random number was "+ random);
+        break;
+    }else if(guess < random){
+        guess = prompt("Hint: Your guess is to small. Please guess again:");
+    }else{
+        guess = prompt("Hint: Your guess is to large. Please guess again:");
+    }
+}
